323188af1c69d0254bc39b0fb9b1eb5d532a2340
GBAleksandrGB <tomilov.alex@gmail.com>
2021-08-24 23:20:03 +0300

    Add tasks 1_1 and 1_2

---
+++ task_1_2.py
@@ -0,0 +1,31 @@
+numbers = []
+sum_numbers = []
+
+for num in range(1, 1001):
+    if num % 2 != 0:
+        numbers.append(num ** 3)
+
+print(numbers)
+
+for el in numbers:
+    sum_number = 0
+
+    while el != 0:
+        sum_number = sum_number + el % 10
+        el = el // 10
+
+    if sum_number % 7 == 0:
+        sum_numbers.append(sum_number)
+
+print(sum_numbers)
+
+for el in numbers:
+    sum_number = 0
+    el = el + 17
+
+    while el != 0:
+        sum_number = sum_number + el % 10
+        el = el // 10
+
+    if sum_number % 7 == 0:
+        print(sum_number, end=', ')
